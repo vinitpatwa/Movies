a742cab79b5b11f2597f60d1d8ba4c4314d1ade5
Vinit Patwa <vpatwa@fb.com>
2014-10-13 13:52:12 -0700

    Initial Commit

---
+++ rottenTomatoesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottenTomatoesDemo
+//
+//  Created by Vinit Patwa on 10/13/14.
+//  Copyright (c) 2014 Vinit Patwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rottenTomatoesDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rottenTomatoesDemo
+//
+//  Created by Vinit Patwa on 10/13/14.
+//  Copyright (c) 2014 Vinit Patwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
